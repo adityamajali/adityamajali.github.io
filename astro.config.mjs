0a58e6c8541ae901a51f2bd785f8e05106992f8d
adityamajali <adityamajali06@gmail.com>
2023-02-28 00:04:55 +0100

    Renamed folder to avoid conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   sitemap: true,
   site: 'https://adityamajali.gitlab.io/',
   outDir: 'public',
-  publicDir: './src/pages',
+  publicDir: 'static',
   integrations: [mdx(), sitemap(), tailwind(),] 
 });
